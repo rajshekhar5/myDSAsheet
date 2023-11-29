7e842deb0bd8d28115f6817040846cdc7312b6ea
Raj Shekhar <rajshekhar2may2002@gmail.com>
2023-11-29 12:31:37 +0530

    first commit

---
+++ Backtracking/NQueens.py
@@ -0,0 +1,13 @@
+# class QueensProblelm:
+
+#     def __init__(self,n):
+#         self.n = n
+#         self.chess_table = [[None for i in range(n)] for j in range(n)]
+    
+#     def print(self):
+#         print(self.chess_table)
+
+
+# if __name__ == '__main__':
+
+
